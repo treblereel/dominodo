7c388caf10bdb83694413f2d4fa5ce263affb8c0
Dmitrii Tikhomirov <chani@me.com>
2019-03-21 20:49:00 +0100

    here

--- dominodo/dominodo-j2cl/src/main/java/org/dominokit/samples/App.java
+++ dominodo/dominodo-j2cl/src/main/java/org/dominokit/samples/App.java
@@ -1,17 +1,9 @@
 package org.dominokit.samples;
 
 import com.google.gwt.core.client.EntryPoint;
-import org.gwtproject.safehtml.shared.annotations.GwtIncompatible;
 
 public class App implements EntryPoint {
 
     public void onModuleLoad() {
-        new DominoDo().run("puppa", "subpuppa");
-    }
-
-
-    @GwtIncompatible()
-    public void emptyPuppa() {
-        System.out.println("PIPPO IS SUPER STRONG");
     }
 }
